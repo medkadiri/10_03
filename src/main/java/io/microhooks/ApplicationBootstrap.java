49389379f902821498d373e1c93a397cd8b8aebe
1Anass <a.zouine@aui.ma>
2022-04-13 01:49:33 +0000

    attempt to remove application bootstrap and make everything at build time - failed

--- src/main/java/io/microhooks/ApplicationBootstrap.java
+++ src/main/java/io/microhooks/ApplicationBootstrap.java
@@ -1,64 +1,71 @@
 package io.microhooks;
 
-import org.springframework.boot.context.event.ApplicationPreparedEvent;
-import org.springframework.context.ApplicationListener;
-
-import io.microhooks.ddd.Source;
-import io.microhooks.ddd.internal.CustomListener;
-import io.microhooks.ddd.internal.SourceListener;
-
-import java.util.Iterator;
+import java.io.IOException;
 
 import javax.persistence.EntityListeners;
 
-import net.bytebuddy.ByteBuddy;
-import net.bytebuddy.agent.ByteBuddyAgent;
 import net.bytebuddy.description.annotation.AnnotationDescription;
-import net.bytebuddy.dynamic.loading.ClassReloadingStrategy;
+import net.bytebuddy.description.type.TypeDescription;
+import net.bytebuddy.dynamic.ClassFileLocator;
+import net.bytebuddy.dynamic.DynamicType;
 
-import org.atteo.classindex.ClassIndex;
+public class ApplicationBootstrap implements net.bytebuddy.build.Plugin {
 
-// import io.microhooks.ddd.internal.Trackable;
-
-public class ApplicationBootstrap implements ApplicationListener<ApplicationPreparedEvent> {
-
-        public void onApplicationEvent(ApplicationPreparedEvent ev) {
-                System.out.println("xxxxxxxxxxxxxxxxxxxxxxxxxxx");
-
-                ByteBuddyAgent.install();
-
-                Iterable<Class<?>> sourceClasses = ClassIndex.getAnnotated(Source.class);
-
-                // lookup entity classes with @source
-                ByteBuddy byteBuddy = new ByteBuddy();
-                Iterator<Class<?>> iterator = sourceClasses.iterator();
-
-                if (iterator == null)
-                        System.out.println("Iterator is null");
-
-                while (iterator.hasNext()) {
-                        Class<?> klass = iterator.next();
-                        System.out.println(klass.toString());
-
-                        byteBuddy
-                                        .redefine(klass)
-                                        .annotateType(AnnotationDescription.Builder.ofType(EntityListeners.class)
-                                                        .defineTypeArray("value", SourceListener.class,
-                                                                        CustomListener.class)
-                                                        .build())
-                                        .make()
-                                        .load(klass.getClassLoader(),
-                                                        ClassReloadingStrategy.fromInstalledAgent());
-                }
-                /*
-                 * new ByteBuddy()
-                 * .redefine(TestEntity.class)
-                 * .annotateType(AnnotationDescription.Builder.ofType(EntityListeners.class)
-                 * .defineTypeArray("value", SourceListener.class, CustomListener.class)
-                 * .build())
-                 * .make()
-                 * .load(TestEntity.class.getClassLoader(),
-                 * ClassReloadingStrategy.fromInstalledAgent());
-                 */
+    public static class Load extends ClassLoader{
+        public Class findClass(String name, ClassFileLocator classFileLocator) {
+            byte[] b;
+            try {
+                b = classFileLocator.locate(name).resolve();
+                return defineClass(name, b, 0, b.length);
+            } catch (IOException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+            return null;
         }
+    }
+
+    @Override
+    public
+    boolean matches(TypeDescription target) {
+        return true;
+    }
+
+    @Override
+    public
+    DynamicType.Builder<?> apply(DynamicType.Builder<?> builder, TypeDescription typeDescription, ClassFileLocator classFileLocator) {
+        boolean b = typeDescription.getInheritedAnnotations().toString().contains("@io.microhooks.ddd.Source");
+        Class trackable = null, sourceListener = null, customListener = null;
+
+        try{
+            Load l = new Load();
+            trackable = l.findClass("io.microhooks.ddd.Trackable", classFileLocator);//classFileLocator.locate("io.microhooks.ddd.Trackable").getClass();
+            sourceListener = l.findClass("io.microhooks.ddd.internal.SourceListener", classFileLocator);
+            customListener = l.findClass("io.microhooks.ddd.internal.CustomListener", classFileLocator);
+            //System.out.println("trackable: " + builder.getClass().getCanonicalName());//trackable.toString());
+        }catch(Exception e){
+            System.out.println("e2: " + e);
+        }
+
+        System.out.println("Transforming " + b);
+        if(b){
+            System.out.println("1");
+            System.out.println(trackable);
+            System.out.println("2");
+            //builder = builder.implement(Trackable.class);
+            builder = builder.implement(trackable).annotateType(AnnotationDescription.Builder.ofType(EntityListeners.class)
+                            .defineTypeArray("value", sourceListener,
+                                    customListener).build());//.make().load(trackable.getClassLoader());//defineField("trackedFields", Map<String, Object>, Modifier.PUBLIC | Modifier.TRANSIENT).value(new HashMap<>());
+            System.out.println("Hello " + typeDescription.getDeclaredFields());
+            return builder;//.implement(Trackable.class).defineField("trackedFields", Map<String, Object>, Modifier.PUBLIC);
+        }
+        else
+            return builder;
+    }
+
+    @Override
+    public
+    void close() throws IOException {
+
+    }
 }
